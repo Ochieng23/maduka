fe98085eb9d20fa3a9d9413638cf91b13548e3d1
paul <ochiengpaul001@gmail.com>
2023-11-10 15:52:21 +0300

    version 1.2

--- libs/ui-components/src/lib/navigation.tsx
+++ libs/ui-components/src/lib/navigation.tsx
@@ -27,8 +27,7 @@ const navigation = {
           href: '#',
           imageSrc:
             'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
-          imageAlt:
-            'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+          imageAlt: 'Close up black tees.',
         },
       ],
       sections: [
